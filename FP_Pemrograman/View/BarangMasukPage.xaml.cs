10775730c8e0609a032c2d407c40f6bcd3c3139a
athariq1234@gmail.com <athariq1234@gmail.com>
2022-01-04 20:17:41 +0800

    - add barang masuk page

---
+++ FP_Pemrograman/View/BarangMasukPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FP_Pemrograman.View
+{
+    /// <summary>
+    /// Interaction logic for BarangMasukPage.xaml
+    /// </summary>
+    public partial class BarangMasukPage : Page
+    {
+        public BarangMasukPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
