19689f6619561c33c670a43ba7db4e22c86f578a
Al-Azam <al.azam@students.amikom.ac.id>
2022-01-05 17:32:19 +0700

    update insert barang

---
+++ FP_Pemrograman/Controller/Barang.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FP_Pemrograman.Controller
+{
+    internal class Barang
+    {
+        Model.BarangModel barang;
+        View.BarangMasukPage barangpage;
+
+        public Barang(View.BarangMasukPage barangpage)
+        { 
+            barang = new Model.BarangModel();
+            this.barangpage = barangpage;
+        }
+
+        public void Insert()
+        {
+            barang.tanggal = barangpage.dtpTglMsk.Text;
+            barang.id_barang = barangpage.txtIdBrng.Text;
+            barang.nama_barang = barangpage.txtNamaBrng.Text;
+            barang.harga = barangpage.txtHargaBrng.Text;
+
+            bool result = barang.InsertBarang();
+            if (result)
+            {
+                MessageBox.Show("Menginputkan barang baru berhasil");
+            }
+            else
+            {
+                MessageBox.Show("Maaf, proses input barang baru gagal, silahkan coba lagi");
+            }
+
+        }
+    }
+}
---
+++ FP_Pemrograman/Model/BarangModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FP_Pemrograman.Model
+{
+    internal class BarangModel
+    {
+        ModelTemplateQuery barang;
+
+        public string id_barang { get; set; }
+        public string id_supplier { get; set; }
+        public string nama_barang { get; set; }
+        public string tanggal { get; set; }
+        public string harga { get; set; }
+
+        public BarangModel()
+        { 
+        barang = new ModelTemplateQuery();
+        }
+
+        public bool InsertBarang()
+        {
+            string data = "'" + id_barang + "','" + id_supplier + "','" + nama_barang + "','" + tanggal + "','" + harga + "'";
+            return barang.Insert("barang", data);
+        }
+}
+}
